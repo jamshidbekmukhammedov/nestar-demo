0d2632caa6d3e72e2238c08c55aa8ea824989bd1
Jamshidbek <jamshidbekmukhammedov@gmail.com>
2026-05-02 15:15:01 +0900

    feat: publish main layout components

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -1,4 +1,4 @@
-import withLayoutBasic from "@/libs/components/LayoutBasic";
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
 import { NextPage } from "next";
 import { useState } from "react";
 
--- pages/property/detail.tsx
+++ pages/property/detail.tsx
@@ -1,4 +1,4 @@
-import withLayoutBasic from "@/libs/components/LayoutBasic";
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
 import { Container, Stack } from "@mui/material";
 import { NextPage } from "next";
 
