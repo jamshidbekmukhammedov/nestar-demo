754201de89372badb0443efda7775881133669e2
Jamshidbek <jamshidbekmukhammedov@gmail.com>
2026-05-07 02:39:22 +0900

    feat: develop mobile version

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,17 +9,25 @@ import { NextPage } from "next";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
+import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
 
 const Home: NextPage = () => {
-  return (
-    <Stack className="home-page">
-      <TrendProperties />
-      <PopularProperties />
-      <Advertisement />
-      <TopProperties />
-      <TopAgents />
-    </Stack>
-  );
+  // DEVICE: MOBILE vs PC
+  const device = useDeviceDetect();
+
+  if (device === "mobile") {
+    return <Stack>HOMEPAGE MOBILE</Stack>;
+  } else {
+    return (
+      <Stack className="home-page">
+        <TrendProperties />
+        <PopularProperties />
+        <Advertisement />
+        <TopProperties />
+        <TopAgents />
+      </Stack>
+    );
+  }
 };
 
 export default withLayoutMain(Home);
